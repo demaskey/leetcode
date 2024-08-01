ccc6878406930e10f3fa9e9833aba273cc94133a
Christoph DeMaskey <christoph.demaskey@gmail.com>
2024-08-01 12:27:07 -0400

    Solved problem 2678.

--- leetcode/2678-number-of-senior-citizens.cs
+++ leetcode/2678-number-of-senior-citizens.cs
@@ -2,6 +2,16 @@ namespace leetcode.s2678;
 
 public class Solution {
     public int CountSeniors(string[] details) {
-        return 0;
+        int countSeniors = 0;
+
+        foreach(string passengerDetails in details) {
+            string ageString = passengerDetails.Substring(11,2);
+            int age = int.Parse(ageString);
+
+            if(age > 60) {
+                countSeniors++;
+            }
+        }
+        return countSeniors;
     }
 }
